2e0ea4992a9992be74fdd268bf808f559544dea9
Nabeel <nabeelahmddd@gmail.com>
2022-05-26 01:10:32 +0530

    addded custom password validator

--- mysite/settings.py
+++ mysite/settings.py
@@ -92,12 +92,16 @@ AUTH_PASSWORD_VALIDATORS = [
     },
     {
         'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        'OPTIONS': {
+            'min_length': 8,
+        }
+    },
+    # added custom validators
+    {
+        'NAME': 'backends.validators.NumberValidator',
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+        'NAME': 'backends.validators.SymbolValidator',
     },
 ]
 
